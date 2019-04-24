2374928faecafbe752b47cc37d0f527c680cbe2d
Ramesh VK <ramesh@do_not_email_me.com>
2019-04-23 23:40:08 -0700

    core: add PathChange.merge support

--- core/path_change.js
+++ core/path_change.js
@@ -7,6 +7,7 @@
 import { registerChangeClass } from "./change.js";
 import { decodeChange } from "./change.js";
 import { encode } from "./encode.js";
+import { Replace } from "./replace.js";
 
 // PathChange represents a change at a specific path
 export class PathChange {
@@ -23,14 +24,78 @@ export class PathChange {
   }
 
   revert() {
-    return new PathChange(this.path, this.change.revert());
+    if (this.change == null) {
+      return null;
+    }
+
+    return PathChange.create(this.path, this.change.revert());
+  }
+
+  reverseMerge(other) {
+    if (this.path === null || this.path.length === 0) {
+      const [left, right] = other.merge(this.change);
+      return [right, left];
+    }
+
+    if (other instanceof Replace) {
+      const before = other.before.apply(this);
+      return [new Replace(before, other.after), null];
+    }
+    throw "unexpected PathChange.reverseMerge";
+  }
+
+  merge(other) {
+    if (other == null) {
+      return [null, this];
+    }
+
+    if (this.change == null) {
+      return [other, null];
+    }
+
+    if (this.path == null) {
+      return this.change.merge(other);
+    }
+
+    if (!(other instanceof PathChange)) {
+      other = new PathChange(null, other);
+    }
+
+    const len = PathChange._commonPrefixLen(this.path, other.path);
+    const ownLen = (this.path && this.path.length) || 0;
+    const otherLen = (other.path && other.path.length) || 0;
+
+    if (len != ownLen && len != otherLen) {
+      return [other, this];
+    }
+
+    if (len == ownLen && len == otherLen) {
+      const [left, right] = this.change.merge(other.change);
+      return [
+        PathChange.create(other.path, left),
+        PathChange.create(this.path, right)
+      ];
+    }
+
+    if (len == ownLen) {
+      const [left, right] = this.change.merge(
+        PathChange.create(other.path.slice(len), other.change)
+      );
+      return [
+        PathChange.create(this.path, left),
+        PathChange.create(this.path, right)
+      ];
+    }
+
+    const [left, right] = other.merge(this);
+    return [right, left];
   }
 
   applyTo(value) {
     if (this.path === null || this.path.length === 0) {
       return value.apply(this.change);
     }
-    return value.apply(this);
+    throw "Unexpected use of PathChange.applyTo";
   }
 
   toJSON() {
@@ -55,6 +120,33 @@ export class PathChange {
     }
 
     const change = decodeChange(decoder, json[1]);
+    return new PathChange(path, change);
+  }
+
+  static _commonPrefixLen(p1, p2) {
+    if (p1 == null || p2 == null) {
+      return 0;
+    }
+    let len = 0;
+    for (; len < p1.length && len < p2.length; len++) {
+      if (JSON.stringify(encode(p1[len])) != JSON.stringify(encode(p2[len]))) {
+        return len;
+      }
+    }
+    return len;
+  }
+
+  static create(path, change) {
+    if (path == null || path.length == 0) {
+      return change;
+    }
+    if (change == null) {
+      return null;
+    }
+    if (change instanceof PathChange) {
+      const otherPath = change.path || [];
+      return this._pc(path.concat(otherPath), change.change);
+    }
     return new PathChange(path, change);
   }
 }
--- test/core/path_change_test.js
+++ test/core/path_change_test.js
@@ -6,17 +6,113 @@
 
 import { expect } from "chai";
 
-import { Null, Atomic, PathChange, Replace } from "../..";
+import { Null, Atomic, Map, PathChange, Replace } from "../..";
 import { FakeDecoder } from "./decoder_test.js";
 
 describe("PathChange", () => {
   it("reverts", () => {
     const path = [1, "hello"];
+    expect(new PathChange(path).revert()).to.equal(null);
+
     const replace = new Replace(new Null(), new Atomic(new Null()));
     const expected = new Replace(replace.after, replace.before);
     expect(new PathChange(path, replace).revert()).to.deep.equal(
       new PathChange(path, expected)
     );
+  });
+
+  it("merges with null", () => {
+    const c = new PathChange();
+    expect(c.merge(null)).to.deep.equal([null, c]);
+  });
+
+  it("merges with no paths", () => {
+    const before = new Atomic("before");
+    const after1 = new Atomic("after1");
+    const after2 = new Atomic("after2");
+
+    const c1 = new PathChange(null, new Replace(before, after1));
+    const c2 = new PathChange(null, new Replace(before, after2));
+    expect(c1.merge(c2)).to.deep.equal(c1.change.merge(c2.change));
+  });
+
+  it("merges with same paths", () => {
+    const before = new Atomic("before");
+    const after1 = new Atomic("after1");
+    const after2 = new Atomic("after2");
+
+    const p = [1, "hello"];
+    const c1 = new PathChange(p, new Replace(before, after1));
+    const c2 = new PathChange(p, new Replace(before, after2));
+    const [left, right] = c1.change.merge(c2.change);
+    const expected = [PathChange.create(p, left), PathChange.create(p, right)];
+    expect(c1.merge(c2)).to.deep.equal(expected);
+  });
+
+  it("merges with replace", () => {
+    const before = new Atomic("before");
+    const after1 = new Atomic("after1");
+    const after2 = new Atomic("after2");
+
+    const c1 = new PathChange(null, new Replace(before, after1));
+    const c2 = new Replace(before, after2);
+    const expected = c1.change.merge(c2);
+    expect(c1.merge(c2)).to.deep.equal(expected);
+  });
+
+  it("reverse merges with replace", () => {
+    const before = new Atomic("before");
+    const after1 = new Atomic("after1");
+    const after2 = new Atomic("after2");
+
+    const c1 = new PathChange(null, new Replace(before, after1));
+    const c2 = new Replace(before, after2);
+    const [left, right] = c2.merge(c1.change);
+    const expected = [right, left];
+    expect(c1.reverseMerge(c2)).to.deep.equal(expected);
+  });
+
+  it("merges with different paths", () => {
+    const replace = new Replace(new Atomic("before"), new Atomic("after"));
+    const c1 = new PathChange(["one"], replace);
+    const c2 = new PathChange(["deux"], replace);
+    const merged = c1.merge(c2);
+    expect(merged[0]).to.equal(c2);
+    expect(merged[1]).to.equal(c1);
+  });
+
+  it("merges with sub path", () => {
+    const inner = new Map().set("hello", new Atomic("world"));
+    const prefix = ["some", 1];
+
+    const world2bozo = new Replace(new Atomic("world"), new Atomic("bozo"));
+    const c1 = new PathChange(prefix.concat("inner", "hello"), world2bozo);
+
+    const inner2boyo = new Replace(inner, new Atomic("boyo"));
+    const c2 = new PathChange(prefix.concat("inner"), inner2boyo);
+
+    const modifiedInner = inner.set("hello", new Atomic("bozo"));
+    const updated = new Replace(modifiedInner, new Atomic("boyo"));
+    const expected = [new PathChange(prefix.concat("inner"), updated), null];
+
+    expect(c1.merge(c2)).to.deep.equal(expected);
+  });
+
+  it("merges with super path", () => {
+    const inner = new Map().set("hello", new Atomic("world"));
+    const prefix = ["some", 1];
+
+    const world2bozo = new Replace(new Atomic("world"), new Atomic("bozo"));
+    const c1 = new PathChange(prefix.concat("inner", "hello"), world2bozo);
+
+    const inner2boyo = new Replace(inner, new Atomic("boyo"));
+    const c2 = new PathChange(prefix.concat("inner"), inner2boyo);
+
+    const modifiedInner = inner.set("hello", new Atomic("bozo"));
+    const updated = new Replace(modifiedInner, new Atomic("boyo"));
+    const expected = [null, new PathChange(prefix.concat("inner"), updated)];
+
+    expect(c2.merge(c1)).to.deep.equal(expected);
   });
 });
 
